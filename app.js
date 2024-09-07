7548869b070c372b9fae23adb266391987d75cf7
Igor Leandro <106167617+IgorLRL@users.noreply.github.com>
2024-09-06 23:59:46 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+function pesquisar() {
+    // Seleciona a seção onde os resultados da pesquisa serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    console.log(section);
+  
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+  
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+      // Constrói o HTML para cada resultado da pesquisa
+      resultados += `
+        <div class="item-resultado">
+          <h2>
+            <a href="#" target="_blank">${dado.titulo}</a>
+          </h2>
+          <p class="descricao-meta">${dado.descricao}</p>
+          <a href=${dado.link} target="_blank">Mais Informações</a>
+        </div>
+      `;
+    }
+  
+    // Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;
+  }
+
---
+++ dados.js
@@ -0,0 +1,19 @@
+let dados = [
+    {
+        titulo : "Rayssa Leal",
+        descricao : "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu estilo único e carisma, ela conquistou o mundo do skate e inspirou uma nova geração de atletas.",
+        link : "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que conquistou a medalha de ouro nos Jogos Olímpicos de Paris. Sua trajetória é marcada por superação e dedicação, e ela se tornou um símbolo de força e empoderamento para as mulheres negras no esporte.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta artística brasileira que conquistou o coração dos brasileiros e se tornou um dos maiores nomes do esporte nacional. Com sua força, determinação e talento, ela coleciona medalhas em competições nacionais e internacionais.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    }
+]
+
+
